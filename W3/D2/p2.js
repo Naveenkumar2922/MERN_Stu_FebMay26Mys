52d56ec4a9384dc1aacf86b58c39a1368df4970b
Naveenkumar2922 <naveenthegr82242@gmail.com>
2026-03-03 14:37:03 +0530

    JS: Comparison operators

---
+++ W3/D2/p2.js
@@ -0,0 +1,12 @@
+//Comparison operators
+const x=5, y=2
+console.log("comparison operators")
+console.log("x==y:",x==y);//loose equality
+console.log("x===y:",x===y);//strict equality
+console.log("x!=y:",x!=y);//loose inequality
+console.log("x!==y:",x!==y);// strict inequality
+console.log("x>3:",x>3);//
+console.log("x<3:",x<3);//
+console.log("x>3:",x>3);//
+console.log("x>=y:",x>=y);//
+console.log("x<=y:",x<=y);//
