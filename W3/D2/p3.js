657056d99ad943ead37b04d798962b3b0cd86a72
Naveenkumar2922 <naveenthegr82242@gmail.com>
2026-03-03 14:37:33 +0530

    JS : logical operators

---
+++ W3/D2/p3.js
@@ -0,0 +1,6 @@
+// logical operators
+const a=true,b=false;
+console.log("logical operators");
+console.log("a&&b=",a&&b);//logical AND
+console.log("a||b=",a||b);//logical OR
+console.log("!a",!a);// logical NOT
