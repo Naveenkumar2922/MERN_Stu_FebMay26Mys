b551d441190dbd7f05b77e574d81f6b1d08e6b18
Naveenkumar2922 <naveenthegr82242@gmail.com>
2026-03-03 14:36:40 +0530

    JS: arithmetic opertors

---
+++ W3/D2/p1.js
@@ -0,0 +1,13 @@
+// arithmetic opertors
+console.log("arithmetic operators");
+const a=4,b=3;
+console.log("a+b",a+b); //Addition
+console.log("a-b",a-b);// Subtraction
+console.log("a*b",a*b);//Multipication
+console.log("a/b",a/b);//Division
+console.log("a%b",a%b);//Modulus
+console.log("a**b",a**b);//Exponential
+console.log("a++",a++);// post-increment
+console.log("++a",++a); //pre-increment
+console.log("a--",a--);// post-decrement
+console.log("--a",--a);// pre-decrement
