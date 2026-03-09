abe7f6bb954d29ad453e65f2c9d71a7f5515c655
Naveenkumar2922 <naveenthegr82242@gmail.com>
2026-03-09 09:10:07 +0530

    event object in clicks

---
+++ W3/D5/p2.js
@@ -0,0 +1,14 @@
+// 
+const box = document.getElementById("box");
+const insideBtn = document.getElementById("insideBtn");
+insideBtn.addEventListener("click",function(event){
+    console.log("event type",event.type);
+    console.log("eventt target id",event.target.id);
+    console.log("event current target id,'event.currenttarget.Id");
+});
+
+insideBtn.addEventListener("click",function(event){
+    console.log("event type",event.type);
+    console.log("eventt target id",event.target.id);
+    console.log("event current target id,'event.currenttarget.Id");
+});
