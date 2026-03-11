2b56c9b7ea8f8a844daf1e81c9987395e74adb08
Naveenkumar2922 <naveenthegr82242@gmail.com>
2026-03-11 12:33:08 +0530

    aving and reading the session storage

---
+++ W4/D3/p3.js
@@ -0,0 +1,29 @@
+// Saving and reading the session storage
+const themeInput = document.getElementById("themeInput");
+const output = document.getElementById("output");
+
+document.getElementById("saveBtn").addEventListener("click",function(){
+    sessionStorage.setItem("theme",themeInput.value);
+   sessionStorage.setItem("username","Naveen");
+    sessionStorage.setItem("loggedIn","true");
+    console.log("Saved theme: ",themeInput);
+    output.innerText = "successfully stored to sessionStorage ";
+    output.style.color = "green";
+});
+document.getElementById("readBtn").addEventListener("click",function(){
+    const theme = sessionStorage.getItem("theme");
+    output.innerText = "theme is : " + theme;
+    output.style.color = "green";
+});
+
+document.getElementById("removeBtn").addEventListener("click",function(){
+    sessionStorage.removeItem("loggedIn");
+    output.innerText = "Removed 'loggedIn' ";
+    output.style.color = "green";
+});
+
+document.getElementById("clearBtn").addEventListener("click",function(){
+   sessionStorage.clear();
+    output.innerText = "All cleared ";
+    output.style.color = "green";
+});
