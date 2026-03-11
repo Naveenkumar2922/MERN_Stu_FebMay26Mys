8b5ea84374bc56e827cce2fd5308901a2a1795c7
Naveenkumar2922 <naveenthegr82242@gmail.com>
2026-03-11 12:33:24 +0530

    Storing and retreving JSON data

---
+++ W4/D3/p4.js
@@ -0,0 +1,20 @@
+// Storing and Retreiving JSON data
+const jsonOutput = document.getElementById("jsonOutput");
+
+document.getElementById("saveBtn").addEventListener("click",function(){
+    const user = {
+        id : 102,
+        name : "Naveen",
+        role : "Full stack developers",
+        skills : ["HTML", "CSS", "JS"]
+    };
+    localStorage.setItem("userProfile", JSON.stringify(user));
+    jsonOutput.textContent = "user object saved as string to localStorage";
+});
+
+document.getElementById("readBtn").addEventListener("click",function(){
+    const profile = localStorage.getItem("userProfile");
+    console.log(JSON.parse(up));
+    console.log(up);
+    jsonOutput.textContent = "Profile :" + profile;
+});
