bbf7ab6ae50547d5deb28f6aa2b77e6a208dde31
Naveenkumar2922 <naveenthegr82242@gmail.com>
2026-03-16 16:38:29 +0530

    Introduction to Node.js

--- w5/D1/p1.js
+++ w5/D1/p1.js
@@ -5,10 +5,10 @@ const runtimeName="Node.js";
 console.log(`${runtimeName}runs javascript outside the browser`);
 
 const commonUses=[
-    " used server-side app",
+    " used server-side app",  
     "automation script can be created"
 ];
-// array dsectructuring
+// array dsectructuring 
 // console.log(commonUses[0]);
 // console.log(commonUses[1]);
 commonUses.forEach((commonUse,index)=>{
