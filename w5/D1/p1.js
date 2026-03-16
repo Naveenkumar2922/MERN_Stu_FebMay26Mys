40a21d329c3b78d579b847e38276be12d779de15
Naveenkumar2922 <naveenthegr82242@gmail.com>
2026-03-16 16:25:21 +0530

    ntroduction to Node.js

---
+++ w5/D1/p1.js
@@ -0,0 +1,18 @@
+// Introduction to Node.js
+
+const runtimeName="Node.js";
+// console.log("Introduction to Node.js");
+console.log(`${runtimeName}runs javascript outside the browser`);
+
+const commonUses=[
+    " used server-side app",
+    "automation script can be created"
+];
+// array dsectructuring
+// console.log(commonUses[0]);
+// console.log(commonUses[1]);
+commonUses.forEach((commonUse,index)=>{
+    console.log(`${index+1}.${commonUse}`);
+
+
+});
