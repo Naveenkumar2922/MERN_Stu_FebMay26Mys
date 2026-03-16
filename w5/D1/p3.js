308dddef3a5cf2d3090a8859de222ce6806bddda
Naveenkumar2922 <naveenthegr82242@gmail.com>
2026-03-16 16:26:44 +0530

    setTimeout

---
+++ w5/D1/p3.js
@@ -0,0 +1,9 @@
+console.log("Node js Architecture demo");
+console.log("1.script started");
+
+// setTimeout
+setTimeout(()=>{
+    console.log("3.time callback finished after waiting");
+
+},10000);
+console.log("2.script continued waiting for timer callback");
