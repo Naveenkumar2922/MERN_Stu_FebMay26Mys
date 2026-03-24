8c28a9224a62fb922336f3ebdfc3b046ba92defa
Naveenkumar2922 <naveenthegr82242@gmail.com>
2026-03-24 15:51:24 +0530

    How JS handels asynchronous task in NodeJS

---
+++ W6/D2/p2.js
@@ -0,0 +1,14 @@
+//  How JS handels asynchronous task in NodeJS
+function fetchReport(callback){
+    console.log("Fetching report data...");
+    setTimeout(()=>{
+        const report ="Monthly report is ready";
+        callback(report);
+    },1000);
+}
+fetchReport(function(reportMessage){
+    console.log(reportMessage);
+});
+
+
+console.log("application continues to execute further");
