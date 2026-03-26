62233d08384cb77310c3f2b80f757d6c2ce2682e
Naveenkumar2922 <naveenthegr82242@gmail.com>
2026-03-26 15:55:53 +0530

    Understanding the HTTP module in  NodeJS

---
+++ W6/D4/p1.js
@@ -0,0 +1,10 @@
+//  Understanding the HTTP module in  NodeJS
+
+// Importing built-in http Module
+const http=require("http");
+
+console.log("type of http.createServer:",typeof http.createServer);
+console.log("Common http methods:",http.METHODS);
+
+
+
