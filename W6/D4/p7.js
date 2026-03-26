fea9879926343cadab3260e6bdecc57ae44b13c0
Naveenkumar2922 <naveenthegr82242@gmail.com>
2026-03-26 17:21:35 +0530

    parsing query parameters

---
+++ W6/D4/p7.js
@@ -0,0 +1,24 @@
+// parsing query parameters
+const http= require("http");
+
+const{URL}=require("url");
+const server =http.createServer(function(req,res){
+   const fulUrl= new URL(req.url,"http://localhost:3001");
+   if(req.method=="GET"&& fulUrl.pathname ==="/serach"){
+    const term = fulUrl.searchParams.get("term");
+    const page = fulUrl.searchParams.get("page");
+    res.writeHead(200,{"content-type":"application /json"});
+    res.end (JSON.stringify({
+        route:"/search",
+        term:term,
+        page:page
+    }));
+    return;
+   }
+   res.writeHead(404,{"content-type":"application/json"});
+        res.end(JSON.stringify({sucess:false,message:"Route not found."}));
+});
+server.listen(3001,function(){
+    console.log("server is runinng at http://localhost:3001");
+
+});
