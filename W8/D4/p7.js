9d2dbecdefeec06f364112c4632675de47b2cbfb
Naveenkumar2922 <naveenthegr82242@gmail.com>
2026-04-17 09:53:57 +0530

    Generating using crypto and hashing it using bcrypt

---
+++ W8/D4/p7.js
@@ -0,0 +1,21 @@
+// Generating using crypto and hashing it using bcrypt
+const crypto = require('crypto');
+const bcrypt = require('bcrypt');
+
+function  generateOTP(length=6){
+    return crypto.randomInt(100000,999999).toString();
+}
+async function hashOTP(otp){
+    return await bcrypt.hash(otp,10);//convert readable to unreadable
+}
+async function verifyOTP(input,hash){
+    return await bcrypt.compare(input,hash);
+}
+(async ()=>{
+    const otp=generateOTP();
+    console.log("Generated otp:",otp);
+
+    const hashedOtp = await hashOTP(otp);
+    console.log("hashed otp:",hashedOtp);
+    console.log("verification:",await verifyOTP(otp,hashedOtp));
+})();
