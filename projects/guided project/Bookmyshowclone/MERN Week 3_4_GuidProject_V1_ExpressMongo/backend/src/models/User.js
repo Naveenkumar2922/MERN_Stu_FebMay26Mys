49c49f38351ce79d99ec1e2029ef0bc92f4578c6
Naveenkumar2922 <naveenthegr82242@gmail.com>
2026-04-21 11:35:49 +0530

    UserSchema and model created

---
+++ projects/guided project/Bookmyshowclone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/models/User.js
@@ -0,0 +1,60 @@
+
+const mongoose=require("mongoose");
+const bcrypt = require("bcrypt");
+const userSchema=new mongoose.Schema({
+   
+     name:{
+        type:String,
+        required:[true,"Name is required"],
+        trim:true,
+    },
+    email:{
+        type:String,
+        required:[true,"Email is required"],
+        unique:true,
+        lowercase:true,
+        match:[/^\s+@\s+\.\s+$/,"Please use valid email"],
+        index:true,
+    },
+    password:{
+        type:String,
+        required:[true,"password is required"],
+       minlength:6,
+       select:false,
+    },
+    role:{
+        type:String,
+        enm:["user","admin"],
+        dfault:"user",
+    },
+    isverified:{
+        type:Boolean,
+        deafult:false,
+    },
+},
+    {
+        timestamps:true,
+
+    }
+);
+// Hasing the password before save 
+userSchema.pre("save",async function (){
+    if (!this.isModified("password")) {
+        return;  
+    }
+    try{
+        const saltRounds=10;
+        this.password=await bcrypt.hash(this.password,saltRounds);
+    }
+    catch(error){
+        throw error;
+
+    }
+    });
+
+    // Compare password function 
+    userSchema.methods.comparePassword = async function(enteredPassword){
+        return await bcrypt .compare(enteredPassword,this.password);
+
+    };
+    module.exports=mongoose.model("User",userSchema);
